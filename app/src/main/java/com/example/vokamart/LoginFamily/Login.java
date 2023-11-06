cb30d13d5638f6372592a2b7a8e8758a0031671f
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-11-06 08:09:12 +0700

    login update

--- app/src/main/java/com/example/vokamart/LoginFamily/Login.java
+++ app/src/main/java/com/example/vokamart/LoginFamily/Login.java
@@ -8,20 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.vokamart.Connection.DB_Connection;
-import com.example.vokamart.Navbar;
+
+import com.example.vokamart.Connection.ResponsePackage.APIResponse;
+import com.example.vokamart.Connection.RetrofitClient;
+import com.example.vokamart.Connection.UserPostRequest;
+import com.example.vokamart.MainFamily.home;
 import com.example.vokamart.R;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.io.UnsupportedEncodingException;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Login extends AppCompatActivity {
     @Override
@@ -54,63 +50,47 @@ public class Login extends AppCompatActivity {
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if (!(email.isEmpty() || password.isEmpty())) {
-                    // Hash the user's password before sending it to the server
-                    String hashedPassword = hashPassword(password);
+                createPost(email, password);
 
-                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            }
+        });
 
-                    // Include the hashed password in the URL parameters
-                    String url = DB_Connection.urllogin + "?Email_akun=" + email + "&password=" + hashedPassword;
 
-                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (!(response.equals("Welcome"))) {
-                                Toast.makeText(Login.this, "Berhasil Login", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(), Navbar.class));
-                            } else {
-                                Toast.makeText(Login.this, "Login Gagal", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(Login.this, error.toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
+    }
 
-                    requestQueue.add(stringRequest);
+    private void createPost(String email, String password) {
+
+        RetrofitClient.INSTANCE.getInstance().post(new UserPostRequest(email,password)).enqueue(new Callback<APIResponse>() {
+            @Override
+            public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
+                if(response.isSuccessful()){
+                    APIResponse apiResponse = response.body();
+                    if(apiResponse != null && apiResponse.getStatus().equals("Sukses")){
+
+                        System.out.println("Request success. Response: " + email);
+                        System.out.println("Request success. Response: " + password);
+
+                            Intent i = new Intent(Login.this, home.class);
+                            startActivity(i);
+
+                    } else {
+                        System.out.println("Unexpected response: " + apiResponse);
+                    }
                 } else {
-                    Toast.makeText(Login.this, "Password atau Email salah", Toast.LENGTH_SHORT).show();
+                    System.out.println("Server error, Status Code: " + response.code());
                 }
+            }
+
+            @Override
+            public void onFailure(Call<APIResponse> call, Throwable t) {
+                System.out.println("Request Failed: " + t.getMessage());
+
             }
         });
     }
 
-    // Function to hash the password using SHA-256
-    private String hashPassword(String password) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] bytes = digest.digest(password.getBytes("UTF-8"));
-
-            // Convert the byte array to a hexadecimal string
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : bytes) {
-                String hex = Integer.toHexString(0xFF & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
