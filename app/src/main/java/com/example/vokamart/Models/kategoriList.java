add7b1a6705ffa50a07d413e95ff6a91904cf9e6
Adi Hendra <adihendra1303@gmail.com>
2023-11-23 00:58:00 +0700

    Update kategoriList.java

---
+++ app/src/main/java/com/example/vokamart/Models/kategoriList.java
@@ -0,0 +1,18 @@
+package com.example.vokamart.Models;
+
+public class kategoriList {
+
+    private String nama_kategori;
+
+    public kategoriList(String nama) {
+        this.nama_kategori = nama;
+    }
+
+    public String getNama_kategori() {
+        return nama_kategori;
+    }
+
+    public void setNama_kategori(String nama_kategori) {
+        this.nama_kategori = nama_kategori;
+    }
+}
