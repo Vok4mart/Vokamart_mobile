745b96448965f4ecffb2e6d46b4020743604cbd3
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-11-24 19:25:41 +0700

    image picker can be used

--- app/src/main/java/com/example/vokamart/MainFamily/tambah_produk.java
+++ app/src/main/java/com/example/vokamart/MainFamily/tambah_produk.java
@@ -1,25 +1,29 @@
 package com.example.vokamart.MainFamily;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.vokamart.Adapter.SpinnerAdapter;
-import com.example.vokamart.Manifest;
 import com.example.vokamart.Models.kategoriList;
 import com.example.vokamart.R;
 
@@ -33,7 +37,13 @@ public class tambah_produk extends AppCompatActivity implements AdapterView.OnIt
 
     private ArrayList<kategoriList> kategoriList;
     private SpinnerAdapter adapter;
-    Button btPick;
+    private Button btPick, btnCancel;
+    private ImageView iv1, iv2, iv3, iv4, iv5;
+    private ImageView[] imageViews;
+    private int currentImageIndex = 0;
+    private static final int MAX_IMAGES = 5;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
 
 
     @Override
@@ -43,15 +53,23 @@ public class tambah_produk extends AppCompatActivity implements AdapterView.OnIt
 
         kategoriList = new ArrayList<>();
         Spinner spinner = findViewById(R.id.spinner_kategori);
-        adapter = new SpinnerAdapter(this, kategoriList);
         TextView textView = findViewById(R.id.text_tambah_variasi);
-
         btPick = findViewById(R.id.Btn_Pick);
+        btnCancel = findViewById(R.id.Btn_Cancel);
+        iv1 = findViewById(R.id.img1);
+        iv2 = findViewById(R.id.img2);
+        iv3 = findViewById(R.id.img3);
+        iv4 = findViewById(R.id.img4);
+        iv5 = findViewById(R.id.img5);
+        imageViews = new ImageView[]{iv1, iv2, iv3, iv4, iv5};
 
+        adapter = new SpinnerAdapter(this, kategoriList);
 
         fetchDataFromServer();
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this); // Set the listener for item selection
+
+        pickImage();
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,8 +81,12 @@ public class tambah_produk extends AppCompatActivity implements AdapterView.OnIt
 
     private void pickImage() {
 
-        btPick.setOnClickListener(view ->{
-
+        btPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opengallery();
+                cnclpicture();
+            }
         });
 
     }
@@ -134,5 +156,71 @@ public class tambah_produk extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView<?> parent) {
         // Handle nothing selected if needed
         Log.d("ItemSelected", "Nothing selected");
+    }
+
+    private void opengallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        galleryIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            if (data.getClipData() != null) {
+                // Handle multiple images
+                ClipData clipData = data.getClipData();
+                int itemCount = Math.min(clipData.getItemCount(), MAX_IMAGES - currentImageIndex);
+
+                for (int i = 0; i < itemCount; i++) {
+                    handleImageSelection(clipData.getItemAt(i).getUri());
+                }
+            } else if (data.getData() != null) {
+                // Handle single image
+                handleImageSelection(data.getData());
+            }
+        }
+    }
+
+    private void handleImageSelection(android.net.Uri imageUri) {
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+
+            // Set the selected image to the current ImageView
+            if (currentImageIndex < MAX_IMAGES) {
+                imageViews[currentImageIndex].setImageBitmap(bitmap);
+                currentImageIndex++;
+            } else {
+                Toast.makeText(this, "You can select up to " + MAX_IMAGES + " images", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("ImageSelection", "Error loading image: " + e.getMessage());
+        }
+    }
+
+    private void clearImageView(int index) {
+        if (index >= 0 && index < imageViews.length) {
+            imageViews[index].setImageBitmap(null);
+            // If you also want to reset the currentImageIndex when clearing an image:
+            // currentImageIndex = index;
+        }
+    }
+
+    private void cnclpicture() {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Assume you want to cancel the last selected image
+                if (currentImageIndex > 0) {
+                    currentImageIndex--;
+                    clearImageView(currentImageIndex);
+                }
+            }
+        });
     }
 }
