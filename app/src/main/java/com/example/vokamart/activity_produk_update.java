b5e72b88a7a61fe4b895b3c4bfaece640578f49e
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-11-27 19:36:04 +0700

    Small Changes in layout

---
+++ app/src/main/java/com/example/vokamart/activity_produk_update.java
@@ -0,0 +1,14 @@
+package com.example.vokamart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_produk_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produk_update);
+    }
+}
